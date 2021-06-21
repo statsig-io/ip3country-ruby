de0b3df1ed36c925fddea7cff484109546a8c6a0
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-06-21 13:49:52 -0700

    Add initial implementation

--- lib/country_lookup.rb
+++ lib/country_lookup.rb
@@ -1,3 +1,126 @@
 module CountryLookup
 
+  class Lookup
+    def initialize
+      initialize_from_file
+    end
+
+    def lookup_ip_string(ip_string)
+      if ip_string.nil? || ip_string.empty?
+        puts "empty"
+        return nil
+      end
+      components = ip_string.split('.')
+      if components.length < 4
+        puts "less than 4"
+        return nil
+      end
+
+      ip_number = components[0].to_i * 16777216 +
+        components[1].to_i * 65536 +
+        components[2].to_i * 256 +
+        components[3].to_i
+      puts ip_number
+
+      self.lookup_ip_number(ip_number)
+    end
+
+    def lookup_ip_number(ip_number)
+      index = self.binary_search(ip_number)
+      puts index.to_s
+      cc = @country_codes[index]
+      if cc == '--'
+        puts "--"
+        return nil
+      end
+      puts "cc"
+      cc
+    end
+
+    private
+
+    def binary_search(value)
+      min = 0
+      max = @ip_ranges.length - 1
+      while min < max do
+        mid = (min + max) >> 1
+        puts min.to_s + ' ' + max.to_s + ' ' + mid.to_s
+        puts @ip_ranges[mid]
+        if @ip_ranges[mid] <= value
+          min = mid + 1
+        else
+          max = mid
+        end
+      end
+      min
+    end
+
+    def initialize_from_file
+      unless @country_codes.nil? || @country_codes.length == 0
+        return
+      end
+      @country_codes = Array.new
+      @ip_ranges = Array.new
+      @country_table = Array.new
+
+      File.open(File.dirname(__FILE__) + '/ip_supalite.table') do |file|
+        until file.eof?
+          c1 = file.read(1)
+          c2 = file.read(1)
+
+          @country_table.push(c1 + c2)
+          if (c1 == '*')
+            break
+          end
+        end
+        printed = 0
+        last_end_range = 0
+        until file.eof?
+          count = 0
+          n1 = file.read(1).ord
+          if n1 < 240
+            count = n1
+          elsif n1 == 242
+            n2 = file.read(1).ord
+            n3 = file.read(1).ord
+            count = n2 | (n3 << 8)
+          elsif n1 == 243
+            n2 = file.read(1).ord
+            n3 = file.read(1).ord
+            n4 = file.read(1).ord
+            count = n2 | (n3 << 8) | (n4 << 16)
+          end
+          last_end_range += count * 256
+          cc = file.read(1).ord
+          if printed < 10
+            puts(last_end_range.to_s)
+            printed = printed + 1
+          end
+
+          @ip_ranges.push(last_end_range)
+          @country_codes.push(@country_table[cc])
+        end
+      end
+    end
+  end
+
+  def self.initialize
+    @lookup = Lookup.new()
+    return nil
+  end
+
+  def self.lookup_ip_string(ip_string)
+    if @lookup.nil?
+      @lookup = Lookup.new()
+    end
+    @lookup.lookup_ip_string(ip_string)
+  end
+
+  def self.lookup_ip_number(ip_number)
+    if @lookup.nil?
+      @lookup = Lookup.new()
+    end
+    @lookup.lookup_ip_number(ip_number)
+  end
+
 end
--- test/test_hardcoded.rb
+++ test/test_hardcoded.rb
@@ -1,9 +1,63 @@
 require 'minitest'
 require 'minitest/autorun'
+require 'country_lookup'
 
 class TestStatsig < Minitest::Test
 
+  def before_setup
+    super
+    CountryLookup.initialize
+  end
+
   def test_ip3country_hardcoded
-    assert { true == true }
+    assert (CountryLookup.lookup_ip_string('1.1.1.1') == 'US')
+    assert (CountryLookup.lookup_ip_string('2.2.2.2') == 'FR')
+    assert (CountryLookup.lookup_ip_string('3.3.3.3') == 'US')
+    assert (CountryLookup.lookup_ip_string('4.4.4.4') == 'US')
+    assert (CountryLookup.lookup_ip_string('5.5.5.5') == 'DE')
+    assert (CountryLookup.lookup_ip_string('6.6.6.6') == 'US')
+    assert (CountryLookup.lookup_ip_string('7.7.7.7') == 'US')
+    assert (CountryLookup.lookup_ip_string('8.8.8.8') == 'US')
+    assert (CountryLookup.lookup_ip_string('9.9.9.9') == 'US')
+    assert (CountryLookup.lookup_ip_string('11.11.11.11') == 'US')
+    assert (CountryLookup.lookup_ip_string('12.12.12.12') == 'US')
+    assert (CountryLookup.lookup_ip_string('13.13.13.13') == 'US')
+    assert (CountryLookup.lookup_ip_string('14.14.14.14') == 'JP')
+    assert (CountryLookup.lookup_ip_string('15.15.15.15') == 'US')
+    assert (CountryLookup.lookup_ip_string('16.16.16.16') == 'US')
+    assert (CountryLookup.lookup_ip_string('17.17.17.17') == 'US')
+    assert (CountryLookup.lookup_ip_string('18.18.18.18') == 'US')
+    assert (CountryLookup.lookup_ip_string('19.19.19.19') == 'US')
+    assert (CountryLookup.lookup_ip_string('20.20.20.20') == 'US')
+    assert (CountryLookup.lookup_ip_string('21.21.21.21') == 'US')
+    assert (CountryLookup.lookup_ip_string('22.22.22.22') == 'US')
+    assert (CountryLookup.lookup_ip_string('23.23.23.23') == 'US')
+    assert (CountryLookup.lookup_ip_string('24.24.24.24') == 'US')
+    assert (CountryLookup.lookup_ip_string('25.25.25.25') == 'GB')
+    assert (CountryLookup.lookup_ip_string('26.26.26.26') == 'US')
+    assert (CountryLookup.lookup_ip_string('27.27.27.27') == 'CN')
+    assert (CountryLookup.lookup_ip_string('28.28.28.28') == 'US')
+    assert (CountryLookup.lookup_ip_string('29.29.29.29') == 'US')
+    assert (CountryLookup.lookup_ip_string('30.30.30.30') == 'US')
+    assert (CountryLookup.lookup_ip_string('31.31.31.31') == 'MD')
+    assert (CountryLookup.lookup_ip_string('41.41.41.41') == 'EG')
+    assert (CountryLookup.lookup_ip_string('42.42.42.42') == 'KR')
+    assert (CountryLookup.lookup_ip_string('45.45.45.45') == 'CA')
+    assert (CountryLookup.lookup_ip_string('46.46.46.46') == 'RU')
+    assert (CountryLookup.lookup_ip_string('49.49.49.49') == 'TH')
+    assert (CountryLookup.lookup_ip_string('101.101.101.101') == 'TW')
+    assert (CountryLookup.lookup_ip_string('110.110.110.110') == 'CN')
+    assert (CountryLookup.lookup_ip_string('111.111.111.111') == 'JP')
+    assert (CountryLookup.lookup_ip_string('112.112.112.112') == 'CN')
+    assert (CountryLookup.lookup_ip_string('150.150.150.150') == 'KR')
+    assert (CountryLookup.lookup_ip_string('200.200.200.200') == 'BR')
+    assert (CountryLookup.lookup_ip_string('202.202.202.202') == 'CN')
+    assert (CountryLookup.lookup_ip_string('45.85.95.65') == 'CH')
+    assert (CountryLookup.lookup_ip_string('58.96.74.25') == 'AU')
+    assert (CountryLookup.lookup_ip_string('88.99.77.66') == 'DE')
+    assert (CountryLookup.lookup_ip_string('25.67.94.211') == 'GB')
+    assert (CountryLookup.lookup_ip_string('27.67.94.211') == 'VN')
+    assert (CountryLookup.lookup_ip_string('27.62.93.211') == 'IN')
+
   end
 end
